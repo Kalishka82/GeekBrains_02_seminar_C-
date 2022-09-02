d293688362f343d92c1dc80e55c055af1f03000c
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-02 22:13:08 +0300

    add ex 14

---
+++ ex_14/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Input number: ");
+int number = int.Parse(Console.ReadLine());
+if (number % 7 == 0 & number % 23 == 0)
+    Console.WriteLine("да");
+else Console.WriteLine("нет");
