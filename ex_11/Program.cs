dd15954c3b4046eb415b65dc6252c2f6aeb675c2
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-02 22:05:02 +0300

    add ex 11

---
+++ ex_11/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+int number = new Random().Next(100, 1000);    // number = число
+Console.WriteLine(number);
+
+int digit1 = number / 100;   // digit = цифра
+int digit3 = (number % 10);
+
+Console.Write(digit1);
+Console.Write(digit3);
+// Console.Write($"{number / 100}{number % 10}");
