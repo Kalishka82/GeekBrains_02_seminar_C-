e0d825e96058fea9c047a845e1937c42cf045a01
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-02 22:12:26 +0300

    add ex 12

---
+++ ex_12/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Input first number: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2){
+    int result = number1 % number2;
+
+    if (result == 0) {
+        Console.WriteLine("Кратно");
+    }
+    else Console.WriteLine($"Некратно, {result}");
+}
+
+else {
+    int result = number2 % number1;
+    if (result == 0) {
+        Console.WriteLine("Кратно");
+    }
+    else Console.WriteLine($"Некратно, {result}");
+}
