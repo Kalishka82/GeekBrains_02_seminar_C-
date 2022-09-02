d905e6ad3542781f56725d41e13df00e0a0355f2
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-02 22:14:10 +0300

    add ex 16

---
+++ ex_16/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Input first number: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2){
+    int result = number2 * number2;
+    if (number1 == result) {
+        Console.WriteLine("yes");
+    }
+    else Console.WriteLine("no");
+}
+else {
+    int result = number1 * number1;
+    if (number2 == result) {
+        Console.WriteLine("yes");
+    }
+    else Console.WriteLine("no");
+}
